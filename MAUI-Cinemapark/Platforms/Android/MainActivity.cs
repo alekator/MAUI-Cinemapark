c1f21fcd1ddbf5b7b608d650fc01e516c0b90501
ALekator <60851776+alekator@users.noreply.github.com>
2024-11-15 02:15:17 +0700

    contentView + controlpanel

--- MAUI-Cinemapark/Platforms/Android/MainActivity.cs
+++ MAUI-Cinemapark/Platforms/Android/MainActivity.cs
@@ -2,10 +2,9 @@
 using Android.Content.PM;
 using Android.OS;
 
-namespace MAUI_Cinemapark
+namespace MAUI_Cinemapark;
+
+[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+public class MainActivity : MauiAppCompatActivity
 {
-    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
-    public class MainActivity : MauiAppCompatActivity
-    {
-    }
 }
--- MAUI-Cinemapark/Platforms/iOS/AppDelegate.cs
+++ MAUI-Cinemapark/Platforms/iOS/AppDelegate.cs
@@ -1,10 +1,9 @@
 ﻿using Foundation;
 
-namespace MAUI_Cinemapark
+namespace MAUI_Cinemapark;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
 {
-    [Register("AppDelegate")]
-    public class AppDelegate : MauiUIApplicationDelegate
-    {
-        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-    }
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
 }
