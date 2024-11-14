c476b15ef11e6a0d69f6437b1444abf8712640d4
ALekator <60851776+alekator@users.noreply.github.com>
2024-11-15 02:03:05 +0700

    Selection and NavBar UI

--- MAUI-Cinemapark/Services/TmdbService.cs
+++ MAUI-Cinemapark/Services/TmdbService.cs
@@ -10,7 +10,7 @@ namespace MAUI_Cinemapark.Services
 {
     public partial class TmdbService
     {
-        private const string ApiKey = "738b6d8d99b51339705b1037c981a488"; // generate it from tmdb website
+        //private const string ApiKey = "738b6d8d99b51339705b1037c981a488"; // generate it from tmdb website
         public const string TmdbHttpClientName = "TmdbClient";
 
         private readonly IHttpClientFactory _httpClientFactory;
